9f5e2dd0dd31af7a39513e9e72ed4136d53f51e9
ackermanjayjay <reza23198@gmail.com>
2022-01-30 19:46:42 +0700

    iteration python

---
+++ FlowControl/for.py
@@ -0,0 +1,6 @@
+#Jika input bilangan bulat >3 maka lakukan perulangan 50x
+input_bilangan=int(input("Masukkan Bilangan: "))
+if input_bilangan>3:
+     print(list(range(50)))
+else:
+    print("Tidak lakukan loop")
