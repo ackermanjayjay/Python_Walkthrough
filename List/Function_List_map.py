3361b0ed7c57fbc3c701d10d900a557af3f46f3e
ackermanjayjay <reza23198@gmail.com>
2022-02-04 07:27:16 +0700

    List with map anda split use spaces

---
+++ List/Function_List_map.py
@@ -0,0 +1,20 @@
+# Menggunakan Map
+
+class Listku:
+    elemet=0
+    masukkanList=0
+
+    def input(self):
+      
+       self.elemet=int(input("Masukkan element List nada anda: "))
+
+    def iterasi(self):
+            self.masukkanList=list(map(int,input("\nMasukkan List anda: ").strip().split(' ')))[:self.elemet]
+
+    def output(self):
+            print("\n List anda: ",self.masukkanList)
+
+Cetak=Listku()
+Cetak.input()
+Cetak.iterasi()
+Cetak.output()
