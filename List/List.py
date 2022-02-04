af218c379982b5924dea076e24f6c68af02b7e62
ackermanjayjay <reza23198@gmail.com>
2022-02-04 07:07:07 +0700

    Input user list

---
+++ List/List.py
@@ -0,0 +1,12 @@
+# Declarasi Input user List
+
+listku=[]
+elemet=int(input("Masukkan element List nada anda: "))
+
+# Iterasi 
+for i in range (0,elemet):
+    ele=int(input())
+
+    listku.append(ele)
+
+print(listku)    
